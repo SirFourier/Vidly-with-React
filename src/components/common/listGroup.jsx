f009702305e29a9380623ab6cc556939f4681050
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-15 10:39:27 +0000

    first commit

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ListGroup({ items, activeItem, onItemChange }) {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li
+          key={item._id}
+          className={
+            item === activeItem ? "list-group-item active" : "list-group-item"
+          }
+          onClick={() => onItemChange(item)}
+        >
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ListGroup.propTypes = {
+  items: PropTypes.array.isRequired,
+//   activeItem: PropTypes.object.isRequired,
+  onItemChange: PropTypes.func.isRequired,
+};
+
+export default ListGroup;
