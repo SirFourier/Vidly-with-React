ba82fbe58c5d904d1b529374675bb9f8c47c714c
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-17 14:57:54 +0000

    Added routing for navbar and movie forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Navbar from "./components/navbar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <>
+      <Navbar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Redirect from="/" exact to="/movies" component={Movies} />
+          <Redirect to="/not-found" component={NotFound} />
+        </Switch>
+      </main>
+    </>
   );
 }
 
