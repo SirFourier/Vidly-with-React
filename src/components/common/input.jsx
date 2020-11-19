761f5139b1551a73bac57287a573e651e3e91419
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-19 22:27:12 +0000

    simplified input props using reset spread operator

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,30 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Input({ autoFocus, name, value, label, error, onChange }) {
+function Input({ name, label, error, ...rest }) {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        autoFocus={autoFocus}
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      ></input>
+      <input {...rest} name={name} id={name} className="form-control"></input>
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 }
 
 Input.propTypes = {
-  autoFocus: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  autoFocus: PropTypes.bool.isRequired,
+  value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
