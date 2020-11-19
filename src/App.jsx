cf5082f05593b79d23daac1fa1a2629e65a60e63
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-19 22:25:56 +0000

    added register route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
-import LoginForm from './components/loginForm';
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 function App() {
@@ -14,11 +15,12 @@ function App() {
       <Navbar />
       <main className="container">
         <Switch>
-          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
           <Redirect from="/" exact to="/movies" component={Movies} />
           <Redirect to="/not-found" component={NotFound} />
         </Switch>
