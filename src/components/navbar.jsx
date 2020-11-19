ef7e2caa1cbdb96c238ab5acad3305a8952950ce
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-19 15:11:59 +0000

    prevented navbar from collapsing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,41 +3,40 @@ import { NavLink, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Vidly
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
+    <div className="container">
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Vidly
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
             <NavLink className="nav-link" to="/movies">
               Movies
             </NavLink>
-          </li>
-          <li className="nav-item">
             <NavLink className="nav-link" to="/customers">
               Customers
             </NavLink>
-          </li>
-          <li className="nav-item">
             <NavLink className="nav-link" to="/rentals">
               Rentals
             </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <NavLink className="nav-link" to="/login">
+              Login
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
