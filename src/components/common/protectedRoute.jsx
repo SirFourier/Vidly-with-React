9accd99ca011931b89fd1d7460fab5b980730718
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-28 00:10:20 +0000

    for preventing access to links that require user

---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import auth from "../../services/authService";
+
+const ProtectedRoute = ({ component: Component, render, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return auth.getCurrentUser() ? (
+          Component ? (
+            <Component {...props} />
+          ) : (
+            render(props)
+          )
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
