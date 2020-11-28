174e6d5f1143409d2652aaf0418b48dde368170c
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-11-28 00:05:40 +0000

    promited to class and added user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/navbar";
@@ -9,28 +9,45 @@ import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import ProtectedRoute from "./components/common/protectedRoute";
+import auth from "./services/authService";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <ToastContainer />
-      <Navbar />
-      <main className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies" component={Movies} />
-          <Redirect to="/not-found" component={NotFound} />
-        </Switch>
-      </main>
-    </>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <>
+        <ToastContainer />
+        <Navbar user={user} />
+        <main className="container">
+          <Switch>
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={user} />}
+            />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" component={Movies} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
